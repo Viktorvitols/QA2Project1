9b16e17c3378cbbe6b4d7988058ef64972efcf91
Viktorvitols <viktorvitols@gmail.com>
2019-09-01 13:36:33 +0300

    klassnaja rabota commit

---
+++ src/test/java/OurFirstTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OurFirstTest {
+
+    private Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    @Test  //аннотация
+    public void showSum() {
+        Integer c;
+        Integer d;
+
+        c = sum(15, 5);
+        d = sum(25, 15);
+
+//        System.out.println("C is: " + c + " \nand D is: " + d);
+        Assertions.assertEquals(20, c, "Shit!!!");
+    }
+}
