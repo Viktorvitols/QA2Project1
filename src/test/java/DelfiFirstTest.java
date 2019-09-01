0ad8514c3bf1c0a142c111249d2a65bf83b6a97e
Viktorvitols <viktorvitols@gmail.com>
2019-09-01 14:59:23 +0300

    klassnaja rabota commit

---
+++ src/test/java/DelfiFirstTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+    public class DelfiFirstTest {
+
+        @Test
+        public void delfiFirstTitleTest() {
+            System.setProperty("webdriver.gecko.driver", "c:/drivers/geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            driver.get("http://rus.delfi.lv");
+
+        }
+    }
