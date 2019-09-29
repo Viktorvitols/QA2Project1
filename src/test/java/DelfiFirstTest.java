3a472fa8bf1c568eba06dc3790eb903b6d7849a8
Viktorvitols <viktorvitols@gmail.com>
2019-09-29 23:20:30 +0300

    removed empty lines

--- src/test/java/DelfiFirstTest.java
+++ src/test/java/DelfiFirstTest.java
@@ -3,14 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-    public class DelfiFirstTest {
+public class DelfiFirstTest {
 
-        @Test
-        public void delfiFirstTitleTest() {
-            System.setProperty("webdriver.gecko.driver", "c:/drivers/geckodriver.exe");
-            WebDriver driver = new FirefoxDriver();
-            driver.manage().window().maximize();
-            driver.get("http://rus.delfi.lv");
+    @Test
+    public void delfiFirstTitleTest() {
+        System.setProperty("webdriver.gecko.driver", "c:/drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://rus.delfi.lv");
 
-        }
+
+
     }
+}
