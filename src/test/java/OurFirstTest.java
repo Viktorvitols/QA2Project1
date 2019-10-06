aa96242c9469ac5855cccc86d0594bb0f35c6932
Viktorvitols <viktorvitols@gmail.com>
2019-10-06 15:16:38 +0300

    klassnaja rabota Logging & debugging

--- src/test/java/OurFirstTest.java
+++ src/test/java/OurFirstTest.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -6,16 +8,23 @@ public class OurFirstTest {
     private Integer sum(Integer a, Integer b) {
         return a + b;
     }
+    private final Logger LOGGER = LogManager.getLogger(OurFirstTest.class);
 
     @Test  //аннотация
     public void showSum() {
+        LOGGER.info("We are starting our test");
         Integer c;
         Integer d;
+        LOGGER.info("Now we are summing two digits");
 
         c = sum(15, 5);
+
+        LOGGER.info("And again");
         d = sum(25, 15);
 
-//        System.out.println("C is: " + c + " \nand D is: " + d);
+        LOGGER.info("C is: " + c + " \nand D is: " + d);
+        LOGGER.info("Asserting");
         Assertions.assertEquals(20, c, "Shit!!!");
+        LOGGER.info("Assertions finished");
     }
 }
