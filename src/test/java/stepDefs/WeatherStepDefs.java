6af9c82e8bc0b9b4ae593585c948375af7562fc1
Viktorvitols <viktorvitols@gmail.com>
2019-12-22 15:45:38 +0200

    Weather API test

--- src/test/java/stepDefs/WeatherStepDefs.java
+++ src/test/java/stepDefs/WeatherStepDefs.java
@@ -1,11 +1,44 @@
 package stepDefs;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import model.Response;
+import org.junit.jupiter.api.Assertions;
+import requesters.WeatherRequester;
 
 public class WeatherStepDefs {
-    @Given("Print welcome message")
-    public void print_message() {
-        System.out.println("Our first step!");
+
+    private String city;
+    private Response response;
+
+    @Given("city is: (.*)")        //  (.*) - эквивалент любой строки; (.) - эквивалент любого символа
+    public void set_city(String city) {
+        this.city = city;
+    }
+
+    /*
+    1) send request
+    2) receive response
+    3) map response */
+
+    @When("we are requesting weather")
+    public void request_weather() throws JsonProcessingException {
+        WeatherRequester requester = new WeatherRequester();
+        response = requester.requestWeather(city);
+
+    }
+
+    @Then("lon is (.*)")
+    public void check_lon(Double lon) {
+        Assertions.assertEquals(lon, response.getCoord().getLon(), "Wrong lon");
+    }
+
+    @Then("lat is (.*)")
+    public void check_lat(Double lat) {
+        Assertions.assertEquals(lat, response.getCoord().getLat(), "Wrong lat");
+
     }
 
 }
--- src/test/resources/weather.feature
+++ src/test/resources/weather.feature
@@ -1,3 +1,6 @@
 Feature: We are testing weather API
   Scenario: Weather check
-    Given Print welcome message
+    Given city is: London
+    When we are requesting weather
+    Then lon is -0.13
+    And lat is 51.51
